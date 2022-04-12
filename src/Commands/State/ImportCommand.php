7d95bbaebb3d89f01c8aaee3ea9a4d709a9bc2ad
Abdulmhsen B. A. A <admin@arabcoders.org>
2022-04-13 02:35:32 +0300

    added new flag for import/export for request timeout option.

--- src/Commands/State/ImportCommand.php
+++ src/Commands/State/ImportCommand.php
@@ -61,6 +61,12 @@ class ImportCommand extends Command
                 'Disables the proxy for a comma-separated list of hosts that do not require it to get reached.'
             )
             ->addOption(
+                'timeout',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Set request timeout in seconds'
+            )
+            ->addOption(
                 'servers-filter',
                 's',
                 InputOption::VALUE_OPTIONAL,
@@ -198,6 +204,10 @@ class ImportCommand extends Command
 
             if ($input->getOption('no-proxy')) {
                 $opts['client']['no_proxy'] = $input->getOption('no-proxy');
+            }
+
+            if ($input->getOption('timeout')) {
+                $opts['client']['timeout'] = $input->getOption('timeout');
             }
 
             $server['options'] = $opts;
