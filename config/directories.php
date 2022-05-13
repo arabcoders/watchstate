210a4926078ad4b916bdf148b6a0ce99080c4ce4
Abdulmhsen B. A. A <admin@arabcoders.org>
2022-05-13 08:17:10 +0300

    Added missing crons logs directory.

--- config/directories.php
+++ config/directories.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 return [
     '%(path)/db/archive',
     '%(path)/config',
-    '%(tmpDir)/logs',
+    '%(tmpDir)/logs/crons',
     '%(tmpDir)/cache',
     '%(tmpDir)/profiler',
     '%(tmpDir)/webhooks',
