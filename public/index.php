f3226db8e52953e2bacac50129ffd3cdf2c95040
Abdulmhsen B. A. A <admin@arabcoders.org>
2022-06-09 17:28:50 +0300

    cleaned up public facing index

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use App\Command;
+
 error_reporting(E_ALL);
 ini_set('error_reporting', 'On');
 ini_set('display_errors', 'Off');
@@ -10,7 +12,7 @@ require __DIR__ . '/../pre_init.php';
 
 if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
     print 'Dependencies are missing please refer to the documentation to know more about this.';
-    exit(1);
+    exit(Command::FAILURE);
 }
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -22,26 +24,16 @@ set_error_handler(function (int $number, mixed $error, mixed $file, int $line) {
     }
 
     $message = trim(sprintf('%s: %s (%s:%d)', $number, $error, $file, $line));
-
-    if (env('IN_DOCKER')) {
-        fwrite(STDERR, $message);
-    } else {
-        syslog(LOG_ERR, $message);
-    }
-
-    exit(1);
+    $out = fn($message) => env('IN_DOCKER') ? fwrite(STDERR, $message) : syslog(LOG_ERR, $message);
+    $out($message);
+    exit(Command::FAILURE);
 });
 
 set_exception_handler(function (Throwable $e) {
     $message = trim(sprintf("%s: %s (%s:%d).", get_class($e), $e->getMessage(), $e->getFile(), $e->getLine()));
-
-    if (env('IN_DOCKER')) {
-        fwrite(STDERR, $message);
-    } else {
-        syslog(LOG_ERR, $message);
-    }
-
-    exit(1);
+    $out = fn($message) => env('IN_DOCKER') ? fwrite(STDERR, $message) : syslog(LOG_ERR, $message);
+    $out($message);
+    exit(Command::FAILURE);
 });
 
 (new App\Libs\Initializer())->boot()->runHttp();
