dd5b56869857edbc078266a6cc3710e5fb988f11
Abdulmhsen B. A. A <admin@arabcoders.org>
2022-06-10 17:56:14 +0300

    Fixed tryResponse

--- src/Backends/Common/CommonTrait.php
+++ src/Backends/Common/CommonTrait.php
@@ -4,27 +4,28 @@ declare(strict_types=1);
 
 namespace App\Backends\Common;
 
+use Closure;
 use Throwable;
 
 trait CommonTrait
 {
     /**
-     * This method Wrap $fn() call in to a try catch.
+     * Wrap Closure into try catch response.
      *
-     * @param Context $context
-     * @param callable $fn
+     * @param Context $context Context to associate the call with.
+     * @param Closure(): Response $fn Closure
      *
      * @return Response
      */
-    protected function tryResponse(Context $context, callable $fn): Response
+    protected function tryResponse(Context $context, Closure $fn): Response
     {
         try {
-            return new Response(status: true, response: $fn());
+            return $fn();
         } catch (Throwable $e) {
             return new Response(
                 status: false,
                 error:  new Error(
-                            message:  'Unhandled exception was thrown during [%(backend)] request inspection.',
+                            message:  'Unhandled exception was thrown in [%(client): %(backend)] context.',
                             context:  [
                                           'backend' => $context->backendName,
                                           'client' => $context->clientName,
