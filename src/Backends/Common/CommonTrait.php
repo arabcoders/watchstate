66775d472340f4b7dae5634d8a9394dc81f2fe91
Abdulmhsen B. A. A <admin@arabcoders.org>
2022-06-10 17:33:19 +0300

    Added common response/error objects for inner communication between backend clients and their sub actions.

--- src/Backends/Common/CommonTrait.php
+++ src/Backends/Common/CommonTrait.php
@@ -4,25 +4,40 @@ declare(strict_types=1);
 
 namespace App\Backends\Common;
 
+use Throwable;
+
 trait CommonTrait
 {
     /**
-     * Get Backend Name.
+     * This method Wrap $fn() call in to a try catch.
      *
-     * @return string
-     */
-    public function getName(): string
-    {
-        return $this->name ?? 'CommonName';
-    }
-
-    /**
-     * Get Client Name.
+     * @param Context $context
+     * @param callable $fn
      *
-     * @return string
+     * @return Response
      */
-    public function getClientName(): string
+    protected function tryResponse(Context $context, callable $fn): Response
     {
-        return $this->clientName ?? 'CommonClient';
+        try {
+            return new Response(status: true, response: $fn());
+        } catch (Throwable $e) {
+            return new Response(
+                status: false,
+                error:  new Error(
+                            message:  'Unhandled exception was thrown during [%(backend)] request inspection.',
+                            context:  [
+                                          'backend' => $context->backendName,
+                                          'client' => $context->clientName,
+                                          'exception' => [
+                                              'file' => $e->getFile(),
+                                              'line' => $e->getLine(),
+                                              'kind' => get_class($e),
+                                              'message' => $e->getMessage(),
+                                          ]
+                                      ],
+                            previous: $e
+                        )
+            );
+        }
     }
 }
