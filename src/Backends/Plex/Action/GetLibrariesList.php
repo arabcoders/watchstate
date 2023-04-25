34c5773efc2b89fae1f64f65e56becd5ec38f7b5
Abdulmhsen B. A. A <admin@arabcoders.org>
2023-04-25 06:45:17 +0300

    Restored check for library type in backend:library:list supported flag.

--- src/Backends/Plex/Action/GetLibrariesList.php
+++ src/Backends/Plex/Action/GetLibrariesList.php
@@ -119,12 +119,14 @@ final class GetLibrariesList
             $key = (int)ag($section, 'key');
             $type = ag($section, 'type', 'unknown');
             $agent = ag($section, 'agent', 'unknown');
+            $supportedType = PlexClient::TYPE_MOVIE === $type || PlexClient::TYPE_SHOW === $type;
+
             $builder = [
                 'id' => $key,
                 'title' => ag($section, 'title', '???'),
                 'type' => ucfirst($type),
                 'ignored' => true === in_array($key, $ignoreIds ?? []),
-                'supported' => true === in_array($agent, PlexClient::SUPPORTED_AGENTS),
+                'supported' => $supportedType && true === in_array($agent, PlexClient::SUPPORTED_AGENTS),
                 'agent' => ag($section, 'agent'),
                 'scanner' => ag($section, 'scanner'),
             ];
