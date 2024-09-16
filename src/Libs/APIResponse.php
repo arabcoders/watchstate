631b8f2060c41b90ad93fc6fc06cb1537a15004e
Abdulmhsen B. A. A. <admin@arabcoders.org>
2024-09-16 20:24:03 +0300

    Added Test coverage for APIResponse.

--- src/Libs/APIResponse.php
+++ src/Libs/APIResponse.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Libs;
 
 use App\Libs\Enums\Http\Status;
-use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\StreamInterface as iStream;
 
 /**
  * Class Config
@@ -18,17 +18,63 @@ final readonly class APIResponse
         public Status $status,
         public array $headers = [],
         public array $body = [],
-        public StreamInterface|null $stream = null
+        public iStream|null $stream = null
     ) {
     }
 
+    /**
+     * Check if the response has a stream.
+     *
+     * @return bool True if the response has a stream, false otherwise.
+     */
     public function hasStream(): bool
     {
         return null !== $this->stream;
     }
 
+    /**
+     * Check if the response has a body.
+     *
+     * @return bool True if the response has a body, false otherwise.
+     */
     public function hasBody(): bool
     {
         return !empty($this->body);
+    }
+
+    /**
+     * Check if the response has headers.
+     *
+     * @return bool  True if the response has headers, false otherwise.
+     */
+    public function hasHeaders(): bool
+    {
+        return !empty($this->headers);
+    }
+
+    /**
+     * Get Header from the response headers.
+     *
+     * @param string $key The key to get from the headers.
+     * @param mixed|null $default The default value to return if the key is not found.
+     *
+     * @return mixed The value of the key from the headers. If the key is not found, the default value is returned.
+     */
+    public function getHeader(string $key, mixed $default = null): mixed
+    {
+        return ag($this->headers, $key, $default);
+    }
+
+    /**
+     * Get Parameter from the parsed body content.
+     *
+     * @param string $key The key to get from the body.
+     * @param mixed|null $default The default value to return if the key is not found.
+     *
+     * @return mixed The value of the key from the body. If the key is not found, the default value is returned.
+     */
+    public function getParam(string $key, mixed $default = null): mixed
+    {
+        return ag($this->body, $key, $default);
     }
 }
