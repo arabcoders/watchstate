2445c1e18a36f9f989cc0bb8e5ca3fb70146b6fa
ArabCoders <admin@arabcoders.org>
2025-02-14 20:40:03 +0300

    fixed failing tests

---
+++ src/Libs/Extends/MockHttpClient.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Libs\Extends;
+
+use App\Libs\Enums\Http\Method;
+use Symfony\Contracts\HttpClient\ResponseInterface as iResponse;
+
+/**
+ * RetryableHttpClient proxy. This Class acts as proxy in front of the Symfony RetryableHttpClient.
+ *
+ */
+class MockHttpClient extends \Symfony\Component\HttpClient\MockHttpClient
+{
+    public function request(string|Method $method, string $url, array $options = []): iResponse
+    {
+        if (true === ($method instanceof Method)) {
+            $method = $method->value;
+        }
+
+        return parent::request($method, $url, $options);
+    }
+}
