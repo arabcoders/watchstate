c921430651bc32c5f6b19412afc9ba2796857446
arabcoders <admin@arabcoders.org>
2025-04-03 01:18:04 +0300

    Make config:list user aware

--- src/Commands/Config/ListCommand.php
+++ src/Commands/Config/ListCommand.php
@@ -5,17 +5,30 @@ declare(strict_types=1);
 namespace App\Commands\Config;
 
 use App\Command;
+use App\Libs\Attributes\DI\Inject;
 use App\Libs\Attributes\Route\Cli;
-use App\Libs\Config;
+use App\Libs\Exceptions\RuntimeException;
+use App\Libs\Mappers\Import\DirectMapper;
+use App\Libs\Mappers\ImportInterface as iImport;
 use App\Libs\Options;
 use DateTimeInterface;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
+use Psr\Log\LoggerInterface as iLogger;
+use Symfony\Component\Console\Input\InputInterface as iInput;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface as iOutput;
 
 #[Cli(command: self::ROUTE)]
 final class ListCommand extends Command
 {
     public const string ROUTE = 'config:list';
+
+    public function __construct(
+        #[Inject(DirectMapper::class)]
+        private iImport $mapper,
+        private iLogger $logger
+    ) {
+        parent::__construct();
+    }
 
     /**
      * Configures the command.
@@ -23,48 +36,74 @@ final class ListCommand extends Command
     protected function configure(): void
     {
         $this->setName(self::ROUTE)
-            ->setDescription('List Added backends.')
+            ->setDescription('List user backends.')
+            ->addOption('user', 'u', InputOption::VALUE_REQUIRED, 'Select user.', '')
             ->setHelp('This command list your configured backends.');
     }
 
     /**
      * Runs the command.
      *
-     * @param InputInterface $input The input interface.
-     * @param OutputInterface $output The output interface.
+     * @param iInput $input The input interface.
+     * @param iOutput $output The output interface.
      *
      * @return int The command exit code.
      */
-    protected function runCommand(InputInterface $input, OutputInterface $output): int
+    protected function runCommand(iInput $input, iOutput $output): int
     {
+        $user = $input->getOption('user');
+
+        try {
+            $usersContext = getUsersContext(mapper: $this->mapper, logger: $this->logger);
+        } catch (RuntimeException $e) {
+            $output->writeln(r('<error>ERROR:</error> {message}', [
+                'user' => $user,
+                'message' => $e->getMessage(),
+            ]));
+            return self::FAILURE;
+        }
+
+        if ('' !== ($user = $input->getOption('user'))) {
+            $usersContext = array_filter($usersContext, fn($k) => $k === $user, ARRAY_FILTER_USE_KEY);
+            if (count($usersContext) < 1) {
+                $output->writeln(r('<error>ERROR:</error> {message}', [
+                    'message' => r("User '{user}' not found.", ['user' => $user]),
+                ]));
+                return self::FAILURE;
+            }
+        }
+
         $list = [];
 
-        foreach (Config::get('servers', []) as $name => $backend) {
-            $import = 'Disabled';
+        foreach ($usersContext as $username => $userContext) {
+            foreach ($userContext->config->getAll() as $name => $backend) {
+                $import = 'Disabled';
 
-            if (true === (bool)ag($backend, 'options.' . Options::IMPORT_METADATA_ONLY)) {
-                $import = 'Metadata only';
+                if (true === (bool)ag($backend, 'options.' . Options::IMPORT_METADATA_ONLY)) {
+                    $import = 'Metadata only';
+                }
+
+                if (true === (bool)ag($backend, 'import.enabled')) {
+                    $import = 'Play state & Metadata';
+                }
+
+                $importLastRun = ($date = ag($backend, 'import.lastSync')) ? makeDate($date) : 'No record';
+                $exportLastRun = ($date = ag($backend, 'export.lastSync')) ? makeDate($date) : 'No record';
+
+                $list[] = [
+                    'User' => $username,
+                    'Name' => $name,
+                    'Type' => ucfirst(ag($backend, 'type')),
+                    'Import' => $import,
+                    'Export' => true === (bool)ag($backend, 'export.enabled') ? 'Enabled' : 'Disabled',
+                    'LastImportDate' => ($importLastRun instanceof DateTimeInterface) ? $importLastRun->format(
+                        'Y-m-d H:i:s T'
+                    ) : $importLastRun,
+                    'LastExportDate' => ($exportLastRun instanceof DateTimeInterface) ? $exportLastRun->format(
+                        'Y-m-d H:i:s T'
+                    ) : $exportLastRun,
+                ];
             }
-
-            if (true === (bool)ag($backend, 'import.enabled')) {
-                $import = 'Play state & Metadata';
-            }
-
-            $importLastRun = ($date = ag($backend, 'import.lastSync')) ? makeDate($date) : 'No record';
-            $exportLastRun = ($date = ag($backend, 'export.lastSync')) ? makeDate($date) : 'No record';
-
-            $list[] = [
-                'Name' => $name,
-                'Type' => ucfirst(ag($backend, 'type')),
-                'Import' => $import,
-                'Export' => true === (bool)ag($backend, 'export.enabled') ? 'Enabled' : 'Disabled',
-                'LastImportDate' => ($importLastRun instanceof DateTimeInterface) ? $importLastRun->format(
-                    'Y-m-d H:i:s T'
-                ) : $importLastRun,
-                'LastExportDate' => ($exportLastRun instanceof DateTimeInterface) ? $exportLastRun->format(
-                    'Y-m-d H:i:s T'
-                ) : $exportLastRun,
-            ];
         }
 
         $this->displayContent($list, $output, $input->getOption('output'));
