f3b7b0975d2d432e31c9b351595ef10b956d642d
arabcoders <admin@arabcoders.org>
2025-11-03 18:35:17 +0300

    updated removed keys

--- config/removed.keys.php
+++ config/removed.keys.php
@@ -2,5 +2,5 @@
 
 return [
     // -- old keys that are no longer used and should be removed from existing configs.
-    'backend' => ['webhook'],
+    'backend' => ['webhook', 'options.use_old_progress_endpoint'],
 ];
